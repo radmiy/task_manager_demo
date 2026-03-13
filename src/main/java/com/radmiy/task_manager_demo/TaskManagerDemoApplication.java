f9c833b0c5c01787b025723a325efb51324d3092
Dzmitry Rabykin <radmiyrdv@gmail.com>
2026-03-13 11:41:34 +0300

    Init project

---
+++ src/main/java/com/radmiy/task_manager_demo/TaskManagerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.radmiy.task_manager_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagerDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/radmiy/task_manager_demo/TaskManagerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.radmiy.task_manager_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
